25f934cbce2c1cfde8ef72cb271b8455d6d971aa
maybelisa <73255984+MightyThor-bruh@users.noreply.github.com>
2021-10-31 15:41:48 +0300

    Add files via upload

---
+++ Lab6_2/Lab6_2/Lab6_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+unsigned int q = 0, q0, b, sum_q0 = 0;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Введите значение: \nb = "; cin >> b;
+	cout << "Все меньшие натуральные числа, квадрат суммы цифр которых равен b, представлен ниже: ";
+
+	for (int q = 1; q < b; q++)
+	{
+		q0 = q;
+		while (q0 != 0)
+		{
+			sum_q0 += q0 % 10;
+			q0 /= 10;
+		}
+		if (pow(sum_q0, 2) == b)
+		{
+			cout << "\n" << q;
+		}
+		sum_q0 = 0;
+	}
+}
